6e1dbc7752bdabca3b658a2c80d15eb5981c95c5
himanshudhawale <himanshudhawale2@icloud.com>
2019-11-20 02:05:50 -0500

    ss

--- models/helper.js
+++ models/helper.js
@@ -6,12 +6,12 @@ const Twilio = db.Twilio;
 var Task = {
     message:function(body,callback) {
     console.log(body["message"]);
-    const twilio = await Twilio.find();
+    const twilio = Twilio.find();
     return (twilio, callback);
 },
 
 saveResponse1:function(no,response,callback) {
-    const twilio = await Twilio.findOne({phoneNo : no});
+    const twilio =  Twilio.findOne({phoneNo : no});
     var tt = twilio;
     tt.response1 = response;
     tt.count = 1;
@@ -21,7 +21,7 @@ saveResponse1:function(no,response,callback) {
 },
 
 checkEnrollment(no,callback){
-     const twilio = await Twilio.findOne({phoneNo : no});
+     const twilio =  Twilio.findOne({phoneNo : no});
      return (twilio.count, callback);
 },
 
@@ -31,32 +31,32 @@ getSymptopms: function(row,callback) {
 },
 
 getCount:function(phone,callback){
-     const twilio = await Twilio.findOne({phoneNo : no});
+     const twilio =  Twilio.findOne({phoneNo : no});
      return (twilio,callback);
 },
 
 getCurrentSymptoms: function(phone,callback) {
-    const twilio = await Twilio.findOne({phoneNo : no});
+    const twilio = Twilio.findOne({phoneNo : no});
     return (twilio.symptom,callback);
 },
 
 getResponse1: function(phone,callback) {
-    const twilio = await Twilio.findOne({phoneNo : no});
+    const twilio =  Twilio.findOne({phoneNo : no});
     return (twilio.response1, callback);
 },
 getSymptopmByID: function(id,callback) {
-    const twilio = await Twilio.findOne({phoneNo : no});
+    const twilio =  Twilio.findOne({phoneNo : no});
     return (twilio.symptom, callback);
 },
 enroll:function(no,callback) {
-  const twilio = await Twilio.insertOne({phoneNo : no});
+  const twilio =  Twilio.insertOne({phoneNo : no});
   return (twilio,callback);
 },
 getResponse2: function(phone,callback) {
-    const twilio = await Twilio.findOne({phoneNo : no});
+    const twilio =  Twilio.findOne({phoneNo : no});
     return (twilio.response2, callback);
 },getResponse3: function(phone,callback) {
-    const twilio = await Twilio.findOne({phoneNo : no});
+    const twilio =  Twilio.findOne({phoneNo : no});
     return (twilio.response3, callback);
 }
 
