c168c105fb9578a07e137001a19a1b608703303b
himanshudhawale <himanshudhawale2@icloud.com>
2019-11-20 08:51:00 -0500

    ss

--- routes/myroutes.js
+++ routes/myroutes.js
@@ -99,176 +99,16 @@ router.post('/register' , async (req,res) => {
     }
 
     if (survey) {
+          switch (survey.status) {
 
-    switch(survey.count)
-    {
-      //Enrolled
-            // case "1":
+              case "2":
+                  sendScaleSMS()
+                  break;
 
-                    // let currentList = survey.symptom;
-                    // if(currentList.length<=2)
-                    // {
-                    //     await client.messages.create({
-                    //         to: from,
-                    //         from: '+19067537001',
-                    //         body: "Thank you and see you soon"
-                    //     });
-                    //     // await client.messages.create({
-                    //     //     to : from,
-                    //     //     from : '+19067537001',
-                    //     //     body : currentString})
-                    //     // console.log("5");
-                    //     await surveyModel.findOneAndUpdate({ phoneNo: from },
-                    //         {
-                    //             $set: {
-                    //                 count: null
-                    //             }
-                    //     });
-                    //
-                    // }
-                    // let currentString = "Please indicate your symptom ";
-                    // for(let i=0;i<currentList.length;i++)
-                    // {
-                    //     currentString+= "("+(i+1)+")"+ currentList[i] + ", "
-                    // }
-                    // currentString+= "(0) None";
-                    // await client.messages.create({
-                    //     to : from,
-                    //     from : '+19067537001',
-                    //     body : currentString})
-                    // console.log("5");
-                    // await surveyModel.findOneAndUpdate({phoneNo : from},
-                    //     {
-                    //         $set:{
-                    //             count : "2"
-                    //
-                    //         }
-                    //     });
-                    // console.log("6");
-                    // break;
-
-// symptom
-            case "2":
-                let currentList1 = survey.symptom;
-                // let symp = currentList1[messageBody-1];
-                // let currentMap = survey.responseMap;
-                // currentMap.set(symp, "-1");
-                // let ss = currentList1.filter(e => e !== symp);
-
-                if (messageBody == 0) {
-                await client.messages.create({
-                  to: from,
-                  from: '+19067537001',
-                  body: "Thank you and we will check with you later."
-                });
-                await surveyModel.findOneAndUpdate({ phoneNo: from },
-                  {
-                      $set: {
-                          count: null
-                      }
-                  });
-
-                }
-                else if (messageBody > 0 && messageBody <= currentList1.length) {
-                    let symp = currentList1[messageBody - 1];
-                    // currentMap.set(symp, "-1");
-
-                    await client.messages.create({
-                        to: from,
-                        from: '+19067537001',
-                        body: "On a scale from 0 (none) to 4 (severe), how would you rate your " + symp + " in the last 24 hours?"
-                    });
-                    var ss = currentList1.filter(e => e !== symp);
-                    await surveyModel.findOneAndUpdate({ phoneNo: from },
-                        {
-                            $set: {
-                                count: "3",
-                                currentResponse: symp,
-                                symptom: ss
-                        }
-                        });
-                }
-                else {
-                    await client.messages.create({
-                        to: from,
-                        from: '+19067537001',
-                        body: "Please enter a number from 0 to " + currentList1.length
-                    });
-                }
-                // let symp = currentList1[messageBody-1];
-                //
-                // await surveyModel.findOneAndUpdate({phoneNo : from},
-                //         {
-                //             $set:{
-                //                     count : "3",
-                //                     currentResponse: symp,
-                //                     responseMap : currentMap,
-                //                     symptom : ss
-                //             }
-                //         });
-                break;
-
-//scale of symptom
-            case "3":
-            //do not have
-                if(messageBody ==0){
-
-                    await client.messages.create({
-                        to : from,
-                        from : '+19067537001',
-                        body : "You do not have a " + survey.currentResponse});
-
-
-
-                    sendSymptomSMS();
-
-                }
-                //mild
-                else if(messageBody >=1 && messageBody <=2){
-                    await client.messages.create({
-                        to : from,
-                        from : '+19067537001',
-                        body : "You have a mild " + survey.currentResponse});
-
-
-                        sendSymptomSMS();
-
-                    // sendSymptomSMS(survey);
-
-                }
-                //moderate
-                else if(messageBody == 3){
-                    await client.messages.create({
-                        to : from,
-                        from : '+19067537001',
-                        body : "You have a moderate " + survey.currentResponse});
-
-                        sendSymptomSMS();
-
-
-                }
-                //severe
-                else if(messageBody == 4){
-                    await client.messages.create({
-                        to : from,
-                        from : '+19067537001',
-                        body : "You have a severe " + survey.currentResponse});
-
-
-                    sendSymptomSMS();
-
-                }
-                //Edge case
-                else {
-               await client.messages.create({
-                   to: from,
-                   from: '+1906753vey7001',
-                   body: "Please enter a number from 0 to 4"
-               });
-
-           }
-                break;
-    }
+              case "3":
+                  sendReportSMS()
+                  break;
+          }
   }
 
     async function sendSymptomSMS(){
@@ -316,7 +156,94 @@ router.post('/register' , async (req,res) => {
 
     };
 
-//
+    async function sendScaleSMS(){
+      currentList = survey.symptom;
+        if (messageBody == 0) {
+            await client.messages.create({
+                to: from,
+                from: '+19067537001',
+                body: "Thank you and we will check with you later."
+            });
+            await surveyModel.findOneAndUpdate({ phoneNo: from },
+                {
+                    $set: {
+                        count: null
+                    }
+                });
+
+        }
+        else if (messageBody > 0 && messageBody <= currentList.length) {
+            let symp = currentList[messageBody - 1];
+            await client.messages.create({
+                to: from,
+                from: '+19067537001',
+                body: "On a scale from 0 (none) to 4 (severe), how would you rate your " + symp + " in the last 24 hours?"
+            });
+            var ss = currentList.filter(e => e !== symp);
+            await surveyModel.findOneAndUpdate({ phoneNo: from },
+                {
+                    $set: {
+                        count: "3",
+                        currentResponse: symp,
+                        symptom: ss
+                    }
+                });
+        }
+        else {
+            await client.messages.create({
+                to: from,
+                from: '+19067537001',
+                body: "Please enter a number from 0 to " + currentList.length
+            });
+        }
+    };
+
+    async function sendReportSMS(){
+        if (messageBody == 0) {
+            await client.messages.create({
+                to: from,
+                from: '+19067537001',
+                body: "You do not have a " + survey.currentResponse
+            });
+            sendSymptomSMS();
+
+        }
+        else if (messageBody >= 1 && messageBody <= 2) {
+            await client.messages.create({
+                to: from,
+                from: '+19067537001',
+                body: "You have a mild " + survey.currentResponse
+            });
+            sendSymptomSMS();
+        }
+        else if (messageBody == 3) {
+            await client.messages.create({
+                to: from,
+                from: '+19067537001',
+                body: "You have a moderate " + survey.currentResponse
+            });
+            sendSymptomSMS();
+        }
+        else if (messageBody == 4) {
+            await client.messages.create({
+                to: from,
+                from: '+19067537001',
+                body: "You have a severe " + survey.currentResponse
+            });
+            sendSymptomSMS();
+        }
+        else {
+            await client.messages.create({
+                to: from,
+                from: '+19067537001',
+                body: "Please enter a number from 0 to 4"
+            });
+
+        }
+
+
+    };
+
 // async function sendSymptomSMS2(){
 //   let currentList = survey.symptom;
 //
