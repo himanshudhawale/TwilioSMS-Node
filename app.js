7c6f767dcd96b01e3ad42d8df1ed304fef64b416
himanshudhawale <himanshudhawale2@icloud.com>
2019-11-20 02:32:01 -0500

    s

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
